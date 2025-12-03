cc053e7ccc561bbec195ec243a5037c8ff0ffc00
Ana Beatriz Pereira <anas.bp10@gmail.com>
2025-12-03 14:43:21 -0300

    Add asteroid spawning, shooting mechanics, and collision detection with game over on player hit

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import os
 # para não aparecer mensagem de importação do pygame
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 def main():
 
     # prints iniciais de teste
@@ -20,12 +23,17 @@ def main():
     # Create groups first
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     # Set containers before creating player
     Player.containers = (updatable, drawable)
-    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     # Now create player (it will automatically add to groups)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -35,6 +43,21 @@ def main():
         log_state()
         screen.fill("black")
         updatable.update(dt)
+        
+        for asteroid in asteroids:      
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+                    break
+
+        
         for drawable_obj in drawable:
             drawable_obj.draw(screen)
         pygame.display.flip()
